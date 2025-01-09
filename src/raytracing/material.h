fe046f3a46bb7a2d56ebe98c231261f96b3a56ec
Hussein <husenalasady21@gmail.com>
2025-01-09 16:37:16 +0300

    stuff

--- src/raytracing/material.h
+++ src/raytracing/material.h
@@ -5,6 +5,7 @@
 #include "format.h"
 #include "raytracing/ray.h"
 #include "raytracing/geometry.h"
+#include "utils.h"
 
 namespace AiCo
 {
@@ -17,6 +18,8 @@ namespace AiCo
             
             struct scatter_t
             {
+                scatter_t(const ray& out, const color3f& attenuation) : out(out), attenuation(attenuation) {}
+                scatter_t() = delete;
                 const ray out;
                 const color3f attenuation;
             };
@@ -26,9 +29,16 @@ namespace AiCo
 
         class lambertian_diffuse : public material
         {
+        public:
+            color3f albedo = {0, 0, 0};
+            lambertian_diffuse(color3f albedo) : albedo(albedo) {}
+
             [[nodiscard]] virtual std::optional<scatter_t> scatter(const ray& insctRay, geometry::intersection_t insct)const
             {
-
+                glm::vec3 scatterDir = insct.N + randvec_on_unit_sphere();
+                if(nearzero_vec(scatterDir))
+                    scatterDir = insct.N;
+                return scatter_t(ray(scatterDir, insct.P), albedo);
             }
         };
     }
